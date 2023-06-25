a9e54929ccda171780f9534bb1110f5814fd5be7
Nedko Arnaudov <nedko@nedk.org>
2023-06-25 18:40:12 +0300

    gcatroof & ncatroof as multicall variants of catroof

--- src/catroof.lua
+++ src/catroof.lua
@@ -3,34 +3,106 @@
 -- SPDX-FileCopyrightText: Copyright © 2023 Nedko Arnaudov
 -- SPDX-License-Identifier: GPL-3
 
-local lgi = require("lgi")
-local GLib = lgi.GLib
-local Gtk = lgi.Gtk
-local Gio = lgi.Gio
-local dir = Gio.File.new_for_commandline_arg(arg[0]):get_parent()
+local function optparser_run(handle_option_fn)
+  while true do
+    if not arg[1] then break end
+    if arg[1]:match("^-") then
+      handle_option_fn(arg[1])
+      table.remove(arg, 1)
+    else
+      break
+    end
+  end
+end
+
+local progname = string.gsub(arg[0], "(.*/)(.*)", "%2")
+
+opts = {}
+
+-- defaults
+opts.tui = false
+opts.gui = false
+opts.cui = false
+
+if progname == "ncatroof" then
+  opts.tui = true
+elseif progname == "gcatroof" then
+  opts.gui = true
+end
+
+optparser_run(
+  function (opt)
+    --print("option", opt)
+    if opt == "-t" or opt == '-n' then
+      opts.tui = true
+    elseif opt == "-g" then
+      opts.gui = true
+    elseif opt == "-c" then
+      opts.cui = true
+    end
+  end)
+
 local os = require("os")
-local assert = lgi.assert
+local lgi = pcall(function () require("lgi") end)
+local newt = pcall(function () require('newt') end)
 
-local CatRoofApp = lgi.package("CatRoof")
-local class = CatRoofApp:class("AppWindow", Gtk.ApplicationWindow)
+--print(("lgi %q"):format(lgi))
+--print(("newt %q"):format(newt))
 
-function CatRoofApp.AppWindow:_class_init(klass)
-  local f = assert(io.open(dir:get_child('catroof.ui'):get_path(), "r"))
-  local template = f:read("*all")
-  f:close()
+if not opts.tui and not opts.gui and not opts.cui then
+  -- enable gtk+ gui if available
+  if lgi then opts.gui = true end
 
-  local bytes = GLib.Bytes.new(template)
-  klass:set_template(bytes)
+  -- enable newt tui if available
+  if newt then opts.tui = true end
 end
 
-function CatRoofApp.AppWindow:_init(...)
-  self:init_template()
-end
+--print(("tui %q"):format(opts.tui))
+--print(("gui %q"):format(opts.gui))
 
-function CatRoofApp.AppWindow:run()
-  self:show_all()
-  Gtk.main()
-  return 0
-end
+if opts.gui then
+  local lgi = require("lgi")
+  local assert = lgi.assert
+  local GLib = lgi.GLib
+  local Gtk = lgi.Gtk
+  local Gio = lgi.Gio
+  local dir = Gio.File.new_for_commandline_arg(arg[0]):get_parent()
 
-os.exit(CatRoofApp.AppWindow():run())
+  local CatRoofApp = lgi.package("CatRoof")
+  local class = CatRoofApp:class("AppWindow", Gtk.ApplicationWindow)
+
+  function CatRoofApp.AppWindow:_class_init(klass)
+    local f = assert(io.open(dir:get_child('catroof.ui'):get_path(), "r"))
+    local template = f:read("*all")
+    f:close()
+
+    local bytes = GLib.Bytes.new(template)
+    klass:set_template(bytes)
+  end
+
+  function CatRoofApp.AppWindow:_init(...)
+    self:init_template()
+  end
+
+  function CatRoofApp.AppWindow:run()
+    self:show_all()
+    Gtk.main()
+    return 0
+  end
+
+  os.exit(CatRoofApp.AppWindow():run())
+elseif opts.tui then
+  local newt = require('newt')
+
+  newt.Init()
+  newt.Cls()
+
+  newt.PopWindow()
+  newt.OpenWindow(5, 5, 20, 4, "Window 1")
+  newt.Refresh()
+  newt.WaitForKey()
+
+  newt.Finished()
+else
+  print("List of devices:")
+end
